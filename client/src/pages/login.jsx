5b26269cd4800ad0c034930bebf3887ec0b23dd8
Abdelkaderbh <gaderbh2@gmail.com>
2024-04-17 15:01:24 +0100

    ui changes login page

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -5,11 +5,27 @@ import styled from 'styled-components';
 import { useNavigate  } from 'react-router-dom';
 
 const Container = styled.div`
+    margin-top: 5%;
     display: flex;
     justify-content: center;
-    align-items: center;
-    height: 50vh;
+    align-items: flex-start;    
 `;
+
+const Section = styled.div `
+    width: 600px; 
+    z-index: 1000;
+    box-shadow: 0 10px 2px 0 rgba(0, 0, 0, 0.03), 0 1px 6px -1px rgba(0, 0, 0, 0.02), 0 2px 4px 0 rgba(0, 0, 0, 0.02);
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;
+    align-items: center;
+` ;
+
+const Image = styled.image `
+    margin-bottom: 25px; 
+`;
+
+
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -45,23 +61,26 @@ const Login = () => {
 
     return (
         <Container>
-            <Form name="basic" labelCol={{ span: 8, }} wrapperCol={{ span: 16, }} style={{ maxWidth: 600, }} initialValues={{ remember: true, }} autoComplete="off">
-                <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!', },]}>
-                    <Input value={username} onChange={(e) => setUsername(e.target.value)} />
-                </Form.Item>
+            <Section>
+            <Image>
+                <img src="https://themebeyond.com/html/yed/img/images/contact_img.png" alt="" style={{width:'200px'}}/>
+            </Image>
+                <Form name="basic" labelCol={{ span: 8, }} wrapperCol={{ span: 16, }} style={{ maxWidth: 600, }} initialValues={{ remember: true, }} autoComplete="off">
+                    <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!', },]}>
+                        <Input value={username} onChange={(e) => setUsername(e.target.value)} style={{borderRadius:'25px',width:'100%'}} />
+                    </Form.Item>
                 <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!', },]}>
-                    <Input.Password value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <Input.Password value={password} onChange={(e) => setPassword(e.target.value)} style={{borderRadius:'25px',width:'100%'}} />
                 </Form.Item>
-                <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16, }}>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
+        
                 <Form.Item wrapperCol={{ offset: 8, span: 16, }}>
-                    {contextHolder}
-                    <Button type="primary" htmlType="submit" onClick={handleLogin}>
-                        Login
-                    </Button>
-                </Form.Item>
-            </Form>
+                        {contextHolder}
+                        <Button type="primary" htmlType="submit" onClick={handleLogin} style={{width:'50%',backgroundColor:'#3db2ff',borderRadius:'30px 30px 30px 30px'}}>
+                            Login
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Section>
         </Container>
     );
 };
