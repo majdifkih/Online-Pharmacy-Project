bedeb2f2899eabc511656b6f81c7bdcd3f86baf4
Abdelkaderbh <gaderbh2@gmail.com>
2024-05-17 12:15:59 +0100

    delete item from cart (database) commit

--- Server/routes/panierRoute.js
+++ Server/routes/panierRoute.js
@@ -4,5 +4,6 @@ const panierController = require("../controllers/PanierControllers");
 
 router.post("/addpanier", panierController.panier);
 router.get("/getpanier/:id", panierController.getallpalnierbyuser);
+router.delete("/remove", panierController.deleteItemFromPanier);
 
 module.exports = router;
