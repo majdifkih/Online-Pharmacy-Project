d2ddd48ac9b788f21b17945eaf3a35cadbe84c75
majdi <majdi@majdi.com>
2024-05-18 13:35:28 +0100

    protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Profile from './pages/Profile';
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import Login from './pages/login';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Contact from './pages/Contact';
-import {FloatButton } from 'antd';
+import { FloatButton } from 'antd';
 import Register from './pages/Register';
 import Medicaments from './pages/Medicaments';
 import MedicamentDetail from './pages/MedicamentDetail';
@@ -19,25 +19,22 @@ import UserCommandes from './pages/UserCommandes';
 import StockDetail from './pages/Admin/Stock/StockDetail';
 import CommandDetail from './pages/Admin/Command/CommandDetail';
 
-
-
 function App() {
-  const [role,SetRole]=useState(false);
-useEffect(()=>{
-  const token = localStorage.getItem("token");
-  if (token){
-  const decodedToken = jwtDecode(token);
-  const userRole = decodedToken.role;
-  if (userRole === 'admin') {
-    SetRole(true);
-  }
-}
-}, [])
+  const [role, setRole] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const userRole = decodedToken.role;
+      setRole(userRole);
+    }
+  }, []);
 
   return (
     <div>
-        <Routes>
-            <Route exact path="/" element={<Home />} />
+      <Routes>
+      <Route exact path="/" element={<Home />} />
             <Route path="/">   
                  <Route path='login' element={<Login />} />
                  <Route path='register' element={<Register />} />
@@ -45,19 +42,19 @@ useEffect(()=>{
                  <Route path='medics' element={<Medicaments />} />
                  <Route path='about' element={<AboutUs />} />
                  <Route path='contact' element = {<Contact />} />
-                 <Route path='mescommandes' element = {<UserCommandes />} />
-                 <Route path='MedicDetail/:id'  element={<MedicamentDetail />} />
-                 <Route path="dash" element={<Dashboard/>} />
-                 <Route path="commandes" element={<ListCommande/>} />
-                 <Route path="medicaments" element={<Stock/>} />
-                 <Route path="addmedicament" element={<AddStock/>} />
-                 <Route path="editmedicament/:id" element={<EditStock/>} />
-                 <Route path="detailmedicament/:id" element={<StockDetail/>} />
-                 <Route path="detailcommand/:id" element={<CommandDetail/>} />
-            </Route>
-        </Routes>
+                 <Route path="mescommandes" element={role === 'client' ? <UserCommandes />: <p>Authorized</p>} />
+                 <Route path="MedicDetail/:id" element={role === 'admin' ? <MedicamentDetail />: <p>Authorized</p>} />
+                 <Route path="dash" element={role === 'admin' ? <Dashboard /> : <p>Authorized</p>} />
+                 <Route path="commandes" element={role === 'admin' ? <ListCommande /> : <p>Authorized</p>} />
+                 <Route path="medicaments" element={role === 'admin' ?<Stock />: <p>Authorized</p>} />
+                 <Route path="addmedicament" element={role === 'admin' ?<AddStock />: <p>Authorized</p>} />
+                 <Route path="editmedicament/:id" element={role === 'admin' ?<EditStock />: <p>Authorized</p>} />
+                 <Route path="detailmedicament/:id" element={role === 'admin' ?<StockDetail />: <p>Authorized</p>} />
+                 <Route path="detailcommand/:id" element={role === 'admin' ?<CommandDetail />: <p>Authorized</p>} />
+        </Route>
+      </Routes>
 
-        <FloatButton.BackTop />
+      <FloatButton.BackTop />
     </div>
   );
 }
